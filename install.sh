6fc9b45c4d8cb3cc6dca62368552a0334b5356a1
David Mohammed <fossfreedom@users.noreply.github.com>
2015-01-06 21:10:36 +0000

    use $HOME rather than $USER for those users who's home is not home

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ RB=${RB:=true}
 
 SCRIPT_NAME=`basename "$0"`
 SCRIPT_PATH=${0%`basename "$0"`}
-PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/RhythmboxFullscreen/"
+PLUGIN_PATH="${HOME}/.local/share/rhythmbox/plugins/RhythmboxFullscreen/"
 GLIB_SCHEME="org.gnome.rhythmbox.plugins.rhythmboxfullscreen.gschema.xml"
 SCHEMA_FOLDER="schema/"
 GLIB_DIR="/usr/share/glib-2.0/schemas/"
